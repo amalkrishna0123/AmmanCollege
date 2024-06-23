7d4ae33dbb2b5d5ea864f20f70cbbd60894c920a
amalkrishna0123 <amalikka0@gmail.com>
2024-06-23 20:45:43 +0530

    new commit

--- src/components/container/Teacher.jsx
+++ src/components/container/Teacher.jsx
@@ -19,9 +19,9 @@ const Teacher = () => {
       animate={inView ? { opacity: 1, clipPath: 'inset(0 0 0 0)' } : {}} 
       transition={{ duration: 2, ease: [0.42, 0, 0.58, 1] }}  className=' pl-5'>
           <div className=' font-bold sm:text-[1.875rem]  text-[1.5rem] mb-5'>
-            Lorem, ipsum<span className='text-Teal'> Lorem ipsum </span> <br /> Lorem, ipsum dolor.
+          Excellence in <span className='text-Teal'>Nursing Education </span> <br />and Community Services
           </div>
-          <p className=' text-sm leading-7 text-gray mb-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, ducimus quibusdam nulla, optio quam officiis dicta nemo obcaecati sed, dolorem hic blanditiis. Facere nostrum, neque eum saepe sint architecto cum.</p>
+          <p className=' text-sm leading-7 text-gray mb-5'>Our mission is to be a regional center of excellence in nursing education, research, and community services. We aim to provide top-notch educational programs, lead cutting-edge research, and offer vital community services. Our dedicated teachers and staff foster an environment of academic rigor and compassionate care, ensuring our graduates excel in their professional roles.</p>
           <button className='py-3 px-6 bg-Teal text-sm rounded-lg text-white font-medium'>Lorem ipsum</button>
         </motion.div>
         <div className=' p-4 md:w-3/4 sm:row-start-1'>
@@ -35,9 +35,16 @@ const Teacher = () => {
       animate={inView ? { opacity: 1, clipPath: 'inset(0 0 0 0)' } : {}} 
       transition={{ duration: 2, ease: [0.42, 0, 0.58, 1] }}  className=' pl-5'>
           <div className=' font-bold sm:text-[1.875rem]  text-[1.5rem] mb-5'>
-            Lorem <span className='text-Teal'>Lorem ipsum.</span> <br /> Lorem ipsum.
+          Preparing<span className='text-Teal'>Compassionate</span> <br /> and Skilled Nurses
           </div>
-          <p className=' text-sm leading-7 text-gray mb-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, ducimus quibusdam nulla, optio quam officiis dicta nemo obcaecati sed, dolorem hic blanditiis. Facere nostrum, neque eum saepe sint architecto cum.</p>
+          <p className=' text-sm leading-7 text-gray mb-5'>Our vision is to prepare nurses who exemplify excellence in both hospital and community settings, showing dedication, devotion, compassion, and kindness. We offer accessible and affordable high-quality education to students from diverse backgrounds, ensuring all levels of nursing practice are developed. Our teachers and staff support students in becoming skilled nurse practitioners, ready to meet community healthcare needs.
+
+
+
+
+
+
+</p>
           <button className='py-3 px-6 bg-Teal text-sm rounded-lg text-white font-medium'>Get Started</button>
         </motion.div>
         <div className=' p-4 md:w-3/4 sm:row-start-1'>
